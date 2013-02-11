ad162b64b14c56181d9757a0a1e7214fddc8ddcf
Craig M. Stimmel <cstimmel@uw.edu>
2013-02-11 12:30:13 -0800

    Turn form data from the single edit form into json and queue it up for further edits.

--- models.py
+++ models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class QueuedSpace(models.Model):
+    space_id = models.IntegerField(blank=True, null=True)
+    json = models.TextField()
+    q_etag = models.CharField(max_length=40, blank=True)
+    status = models.CharField(max_length=25, blank=True)
+    last_modified = models.DateTimeField(auto_now=True, auto_now_add=True)
--- views/edit_space.py
+++ views/edit_space.py
@@ -1,5 +1,7 @@
+from spacescout_admin.forms import QueueForm
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 import json
@@ -8,22 +10,38 @@ import oauth2
 
 #@login_required
 def edit_space(request, spot_id):
-    #Required settings for the client
-    if not hasattr(settings, 'SS_WEB_SERVER_HOST'):
-        raise(Exception("Required setting missing: SS_WEB_SERVER_HOST"))
-    consumer = oauth2.Consumer(key=settings.SS_WEB_OAUTH_KEY, secret=settings.SS_WEB_OAUTH_SECRET)
-    url = "%s/api/v1/schema" % settings.SS_WEB_SERVER_HOST
-    client = oauth2.Client(consumer)
-    resp, content = client.request(url, 'GET')
-    schema = json.loads(content)
-    spot_url = "%s/api/v1/spot/%s" % (settings.SS_WEB_SERVER_HOST, spot_id)
-    resp, content = client.request(spot_url, 'GET')
-    spot = json.loads(content)
+    if request.POST:
+        space_datum = {}
+        for key in request.POST:
+            space_datum[key] = request.POST[key]
+        data = {'space_id': int(space_datum['id']),
+                'json': json.dumps(space_datum),
+                'status': 'updated', }
+        form = QueueForm(data)
+        if form.is_valid():
+            form.save()
+        else:
+            #TODO: do something appropriate if the form isn't valid
+            pass
+        url = '/space/%s' % space_datum['id']
+        return HttpResponseRedirect(url)
+    else:
+        #Required settings for the client
+        if not hasattr(settings, 'SS_WEB_SERVER_HOST'):
+            raise(Exception("Required setting missing: SS_WEB_SERVER_HOST"))
+        consumer = oauth2.Consumer(key=settings.SS_WEB_OAUTH_KEY, secret=settings.SS_WEB_OAUTH_SECRET)
+        url = "%s/api/v1/schema" % settings.SS_WEB_SERVER_HOST
+        client = oauth2.Client(consumer)
+        resp, content = client.request(url, 'GET')
+        schema = json.loads(content)
+        spot_url = "%s/api/v1/spot/%s" % (settings.SS_WEB_SERVER_HOST, spot_id)
+        resp, content = client.request(spot_url, 'GET')
+        spot = json.loads(content)
 
-    args = {
-        "schema": schema,
-        "spot": spot,
-    }
+        args = {
+            "schema": schema,
+            "spot": spot,
+        }
 
-    context = RequestContext(request, {})
-    return render_to_response('edit_space.html', args, context)
+        context = RequestContext(request, {})
+        return render_to_response('edit_space.html', args, context)
