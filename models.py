63152a62b7ace82a578c63ba9a72fea866c31009
Craig M. Stimmel <cstimmel@uw.edu>
2013-02-13 13:03:26 -0800

    Gave the QueuedSpace model a unicode method that returns something a little more descriptive. Save modified spaces to the queue, and retrieve them from the queue instead of the server on single space edit form if they exist.

--- models.py
+++ models.py
@@ -7,3 +7,6 @@ class QueuedSpace(models.Model):
     q_etag = models.CharField(max_length=40, blank=True)
     status = models.CharField(max_length=25, blank=True)
     last_modified = models.DateTimeField(auto_now=True, auto_now_add=True)
+
+    def __unicode__(self):
+        return "%s (marked %s on %s)" % (self.space_id, self.status, self.last_modified)
--- views/edit_space.py
+++ views/edit_space.py
@@ -1,4 +1,5 @@
 from spacescout_admin.forms import QueueForm
+from spacescout_admin.models import QueuedSpace
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
@@ -13,11 +14,18 @@ def edit_space(request, spot_id):
     if request.POST:
         space_datum = {}
         for key in request.POST:
-            space_datum[key] = request.POST[key]
+            # Since everything's coming back from the form fields as text, try to convert
+            # the strings to the appropriate type
+            space_datum[key] = autoconvert(request.POST[key])
         data = {'space_id': int(space_datum['id']),
                 'json': json.dumps(space_datum),
                 'status': 'updated', }
-        form = QueueForm(data)
+        # if we got the space from the queue, edit it rather than create a new instance
+        if request.POST["q_id"]:
+            q_obj = QueuedSpace.objects.get(pk=int(request.POST["q_id"]))
+            form = QueueForm(data, instance=q_obj)
+        else:
+            form = QueueForm(data)
         if form.is_valid():
             form.save()
         else:
@@ -34,14 +42,32 @@ def edit_space(request, spot_id):
         client = oauth2.Client(consumer)
         resp, content = client.request(url, 'GET')
         schema = json.loads(content)
-        spot_url = "%s/api/v1/spot/%s" % (settings.SS_WEB_SERVER_HOST, spot_id)
-        resp, content = client.request(spot_url, 'GET')
-        spot = json.loads(content)
+
+        try:
+            spot = QueuedSpace.objects.get(space_id=spot_id)
+            q_id = spot.pk
+            spot = json.loads(spot.json)
+        except:
+            spot_url = "%s/api/v1/spot/%s" % (settings.SS_WEB_SERVER_HOST, spot_id)
+            resp, content = client.request(spot_url, 'GET')
+            spot = json.loads(content)
+            q_id = None
 
         args = {
             "schema": schema,
             "spot": spot,
+            "q_id": q_id,
         }
 
         context = RequestContext(request, {})
         return render_to_response('edit_space.html', args, context)
+
+
+def autoconvert(s):
+    """ Takes a string and tries to return a non-string datatype (int, float, list, etc.)
+        If it can't, returns the string.
+    """
+    try:
+        return eval(s)
+    except:
+        return s
