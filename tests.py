06cbf5008e3a3a0815b5160c595bb74613dac929
Sam Olds <samolds@uw.edu>
2013-05-20 12:00:45 -0700

    adds a unit test to test a non manager with correct permission editing a space
    fixes SPOT-822

--- tests.py
+++ tests.py
@@ -5,12 +5,16 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+from django.test.client import Client
 from django.test import TestCase
-from spacescout_admin.utils import *
 from django.core.files import File
 from django.core.files.uploadedfile import UploadedFile
-from spacescout_admin.models import User, QueuedSpace
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import User, Group, Permission
+from spacescout_admin.models import QueuedSpace
+from spacescout_admin.views import edit_space
+from spacescout_admin.forms import QueueForm
+from spacescout_admin.utils import *
+import json
 
 
 class SimpleTest(TestCase):
@@ -55,18 +59,61 @@ class SimpleTest(TestCase):
         self.assertEqual(data, data1)
 
     def test_unauthorized_spot_editing(self):
-        # create a user that doesnt belong to any group
-        self.user1 = User.objects.create(username='user1', password='user1', email='user1@uw.edu')
-        # give user permissions required to edit a spot, generally
-        can_update = Permission.objects.get(name='can_update')
-        self.user1.permissions.add(can_update)
-        # create a spot that has different manager than user
-        before_json = "{'manager': ['test'], 'capacity': 12, 'location': {'floor': '2nd floor', 'latitude': 47.652956, 'room_number': 207, 'longitude': -122.316343, 'building_name': 'Fishery Sciences (FSH)'}}"
-        spot = QueuedSpace.objects.create(space_id=5, json=json_data, errors='{}', status='updated', modified_by='fake', approved_by=None)
-        login(user1)
+        c = Client()
 
-        # as that user, edit some data for that spot
-        after_json = "{'manager': ['test'], 'capacity': 200, 'location': {'floor': 'roof', 'latitude': 47.652956, 'room_number': 1, 'longitude': -3, 'building_name': 'Different'}}"
-        spot.json = after_json
-        #spot.save() ??
-        self.assertEquals(spot.json, before_json)
+        # Create a user
+        self.nonmanager = User.objects.create_user(username='nonmanager', password='pass', email='nonmanager@uw.edu')
+
+        # Give user permissions required to edit a spot, generally, but not a manager of any spot
+        can_update = Permission.objects.get(codename='can_update')
+        self.nonmanager.user_permissions.add(can_update)
+
+        # Create a group that has permission to edit the space
+        test_group = Group.objects.create(name="test")
+
+        # Initialize a spot that has different manager than user
+        before_json = '{"manager": ["test"], "capacity": 12, "location": {"floor": "2nd floor", "latitude": 47.652956, "room_number": 207, "longitude": -122.316343, "building_name": "Fishery Sciences (FSH)"}}'
+        before_queued_space = {
+            "id": 1,
+            "json": before_json,
+            "errors": "{}",
+            "space_etag": "x34C%6v9b&n9x234c48V 5rb-n-[a",
+            "q_etag": None,
+            "status": "updated",
+            "approved_by": None,
+        }
+        before_form = QueueForm(before_queued_space)
+        self.assertEqual(before_form.is_valid(), True, "Makes sure the form is valid")
+        before_space = before_form.save(commit=False)
+        before_space.space_last_modified = "2013-05-08T20:25:09.490751+00:00"
+        before_space.modified_by = self.nonmanager
+        before_space.space_id = 1
+        before_space.save()
+        q_etag = before_space.q_etag
+
+        # Comment out below to not add the user to the group
+        # self.nonmanager.groups.add(test_group)
+
+        # Login the user that doesn't have the correct permissions
+        c.login(username='nonmanager', password='pass')
+
+        # As that user, edit some data for that spot
+        # In the POST request, none of the data gets nested like how the server expects. the data POSTed is flat
+        after_json = {"manager": ["test"], "capacity": 200, "floor": "roof", "q_id": 1, "q_etag": q_etag, "latitude": 47.652956, "room_number": 1, "longitude": -3, "building_name": "Different"}
+        after_queued_space = before_queued_space
+        del after_queued_space["json"]
+        for item in after_json:
+            after_queued_space.update({item: after_json[item]})
+        after_queued_space.update({"space_last_modified": before_space.space_last_modified, "modified_by": before_space.modified_by})
+
+        # Pass different json as a request.POST
+        response = c.post('/space/1', after_queued_space)
+
+        # Log the user out
+        c.logout()
+
+        # Get the spot data
+        space = QueuedSpace.objects.get(space_id=1)
+
+        # Check that the json matches the original and not the modified
+        self.assertEqual(json.loads(space.json), json.loads(before_json), "Make sure the spot data matches the original json and not the data modified by the non-manager.")
