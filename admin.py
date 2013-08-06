9e6ce0e3e2153d6444331950d4c882a0cb4110e6
Craig M. Stimmel <cstimmel@uw.edu>
2013-08-06 12:21:33 -0700

    Startin' from scratch.

--- admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from spacescout_admin.models import QueuedSpace
-
-admin.site.register(QueuedSpace)
--- views/add_multiple.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-
-def add_multiple(request):
-    context = RequestContext(request, {})
-    return render_to_response('add_multiple.html', context)
