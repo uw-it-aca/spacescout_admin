d3f7b1d713658a95b955f3242472a29a292cdeed
Jared Comis <jcomis@uw.edu>
2013-08-08 12:38:28 -0700

    adding edit mockups to space details

---
+++ static/js/main-ck.js
@@ -0,0 +1,1 @@
+$(function(){$(".edit-panel-slide").click(function(e){e.preventDefault();$(".md-modal").addClass("md-show");$("body").addClass("freeze")});$(".edit-panel-close").click(function(e){e.preventDefault();$(".md-modal").removeClass("md-show");$("body").removeClass("freeze")})});
