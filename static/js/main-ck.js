afa5fa36f8e1d831f1387ab8ee5602c838a9e985
Jared Comis <jcomis@uw.edu>
2013-08-15 12:01:03 -0700

    cleaning up details page

--- static/js/main-ck.js
+++
@@ -1,1 +0,0 @@
-$(function(){$(".edit-panel-slide").click(function(e){e.preventDefault();$(".md-modal").addClass("md-show");$("body").addClass("freeze")});$(".edit-panel-close").click(function(e){e.preventDefault();$(".md-modal").removeClass("md-show");$("body").removeClass("freeze")})});
