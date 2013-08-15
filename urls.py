0b3b0c59f11559e4192f785fc37a60ca198e949a
Charlon Palacay <charlon@gmail.com>
2013-08-15 10:08:42 -0700

    Deletes login and updates page headers.

--- urls.py
+++ urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('spacescout_admin.views',
     #url(r'upload/$', 'upload.upload'),
     url(r'^$', 'page.home'),
-    url(r'login/$', 'page.login'),
     url(r'space/$', 'page.space'),
     url(r'edit/$', 'page.edit'),
     url(r'add/$', 'page.add'),
