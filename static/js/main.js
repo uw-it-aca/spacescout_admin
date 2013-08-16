04d1013032f2857d0c7530b759e15e4f19cf5b46
Charlon Palacay <charlon@gmail.com>
2013-08-16 09:19:08 -0700

    Adds hide address bar script.

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,5 @@
 $(function() {
 
-    /*$(".edit-panel-slide").click(function(e) {
-        e.preventDefault();
-        $(".md-modal").addClass('md-show');  
-        $("body").addClass('freeze');  
-    });
-    
-     $(".edit-panel-close").click(function(e) {
-        e.preventDefault();
-        $(".md-modal").removeClass('md-show');  
-        $("body").removeClass('freeze');  
-    });*/
-
     $('#days').change(function(){
         var selected = $(this).val()
         var list = $.map(selected, function(value) {
