b2723de105c5df8713145e378294dd2d2bccf3a6
Charlon Palacay <charlon@gmail.com>
2013-08-29 15:53:30 -0700

    Fixes multi day selection and mocked rows.

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
     
-    // mocking up the multi-day-select for hours editing
+    // handle multi-day selection and display
     $('.day-select').change(function(){
         var selected = $(this).val();
 
         var list = $.map(selected, function(value) {
             return(value);
         });
-        $(".show-days").html(list.join(""));
+        
+        $(this).siblings(".show-days").html(list.join(""));
     });
     
 });
