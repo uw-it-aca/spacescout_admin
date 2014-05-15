3a8543aea04be6195585a16b3c681d0f19ae8755
Patrick Michaud <pmichaud@uw.edu>
2014-05-15 10:44:00 -0700

    Adding a (failing) test for spot-1871

--- tests.py
+++ tests.py
@@ -1,10 +1,20 @@
+""" Copyright 2014 UW Information Technology, University of Washington
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
 
-Replace this with more appropriate tests for your application.
-"""
+from django.utils import unittest
 
-from django.test import TestCase
+# from spacescout_admin.test.spot_1277 import Spot1277
+from spacescout_admin.test.spot_1871 import Spot1871
 
-# Provide your unit tests here.
