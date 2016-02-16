f94b48028465bee864eb56572e80c25bbcd207b9
Craig M. Stimmel <cstimmel@uw.edu>
2016-02-16 13:05:40 -0800

    Rely more on setup.py than requirements.txt.

--- setup.py
+++ setup.py
@@ -5,5 +5,15 @@ from distutils.core import setup
 setup(name='SpaceScout-Admin',
       version='1.0',
       description='Admin app for SpaceScout',
-      install_requires=['Django==1.4.5','django-compressor','django-verbatim','django-mobility','oauth2','urllib3','poster','PIL','simplejson','south'],
+      install_requires=[
+                        'Django>=1.4,<1.5',
+                        'django-compressor<2.0',
+                        'django-verbatim',
+                        'django-mobility',
+                        'oauth2<=1.5.211',
+                        'urllib3',
+                        'poster',
+                        'Pillow',
+                        'simplejson>=2.1',
+                        'south'],
      )
